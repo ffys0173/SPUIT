f4a1a16ee003b68043e23a59218c195f463726b5
Laiel4 <ejaqu4@naver.com>
2019-11-23 13:20:19 +0900

    crawling

---
+++ src/main/java/com/web2019/spuit/otherClasses/Crawling.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Crawling {   
+    private final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
+
+    // SSL
+    public static void setSSL() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        } };
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new SecureRandom());
+        HttpsURLConnection.setDefaultHostnameVerifier(
+            new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            }
+        );
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+
+    public static void main(String[] args) {
+        try {
+            // URL def
+            String connUrl = "http://imnews.imbc.com/index_pc.html";
+            // SSL chk
+            try {
+                if(connUrl.indexOf("https://") >= 0){
+                    Crawling.setSSL();
+                }
+            } catch (Exception e) {
+               
+            }
+
+            // HTML get
+            Connection conn = Jsoup
+                    .connect(connUrl)
+                    .header("Content-Type", "application/json;charset=UTF-8")
+                    .userAgent(USER_AGENT)
+                    .method(Connection.Method.GET)
+                    .ignoreContentType(true);
+
+            Document doc = conn.get();
+
+            // HTML Document Look
+            System.out.println(doc.toString());
+
+        } catch (IOException e) {
+            // Exp : Connection Fail
+            e.printStackTrace();
+        }
+    }
+}
